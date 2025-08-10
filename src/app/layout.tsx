e4c8db57e8bf4e1e3087ba4651e1ec88deda68fe
sayak biswas <sayakbiswas@sayaks-MacBook-Air.local>
2025-08-10 16:39:52 +0530

    seventh commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 
 import "../styles/index.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 export default function RootLayout({
@@ -17,6 +18,7 @@ export default function RootLayout({
 
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
