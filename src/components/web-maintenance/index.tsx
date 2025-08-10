a39343ea368692d6a34919573afec573e8e1439c
sayak biswas <sayakbiswas@sayaks-MacBook-Air.local>
2025-08-10 16:26:16 +0530

    fourth commit

--- src/components/web-maintenance/index.tsx
+++ src/components/web-maintenance/index.tsx
@@ -1,11 +1,10 @@
 
-import Breacrumb from '@/common/Breacrumb';
+
 import HeaderOne from '@/layouts/headers/HeaderOne';
 import React from 'react';
 import Cta2Area from '../homes/home/Cta2Area';
 import FooterOne from '@/layouts/footers/FooterOne';
 import ServiceDetailsArea from './WebMaintenance';
-import Breacrumbwebdev from '@/common/Breacrumbwebdev';
 import Breacrumbwebmain from '@/common/Breacrumbwebmain';
 
 const WebDevServices = () => {
