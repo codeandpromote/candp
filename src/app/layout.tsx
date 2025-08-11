3fe6d7ba6bbdc69cf951257efa5ef726104af664
sayak biswas <sayakbiswas@sayaks-MacBook-Air.local>
2025-08-11 14:38:00 +0530

    thirteen commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y4T5WLGW9C"></script>
+<script id="google-analytics">
+  {`
+    window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Y4T5WLGW9C');
+  
+  `}
+ 
+</script>
+      </head>
       <body>
         <head>
           <link  rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Unbounded:wght@200..900&display=swap" />
